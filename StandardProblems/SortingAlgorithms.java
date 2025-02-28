f9dd84be0cd3ed9749d42592abf8c775c2fd51b5
Rajnish-J <rajnishofficial02@gmail.com>
2025-02-28 07:00:36 +0530

    FIX: selection sort done

--- StandardProblems/SortingAlgorithms.java
+++ StandardProblems/SortingAlgorithms.java
@@ -10,22 +10,22 @@ public class SortingAlgorithms {
         System.out.println("Original array: " + Arrays.toString(arr));
 
         // * Call bubble sort and print result
-        bubbleSort(arr);
-        System.out.println("Sorted array using bubble sort: " + Arrays.toString(arr));
+        int[] sortedBubbleSort = bubbleSort(arr);
+        System.out.println("Sorted array using bubble sort: " + Arrays.toString(sortedBubbleSort));
 
         // * Reset the array to original order
         arr = new int[]{45, 10, 18, 17, 7};
 
         // * Call selection sort and print result
-        selectionSort(arr);
-        System.out.println("Sorted array using selection sort: " + Arrays.toString(arr));
+        int[] sortedSelectionSort =  selectionSort(arr);
+        System.out.println("Sorted array using selection sort: " + Arrays.toString(sortedSelectionSort));
 
     }
 
-    public static void bubbleSort(int[] arr) {
+    public static int[] bubbleSort(int[] arr) {
         int size = arr.length;
-        for (int i = 0; i < size - 1; i++) { // Fixed loop condition
-            for (int j = 0; j < size - i - 1; j++) { // Prevent out-of-bounds access
+        for (int i = 0; i < size - 1; i++) { 
+            for (int j = 0; j < size - i - 1; j++) { 
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -33,23 +33,25 @@ public class SortingAlgorithms {
                 }
             }
         }
+        return arr;
     }
 
-    public static void selectionSort(int[] arr) {
+    public static int[] selectionSort(int[] arr) {
         int size = arr.length;
         int minIndex;
-        for (int i = 0; i < size - 1; i++) { // Fixed loop condition
+        for (int i = 0; i < size - 1; i++) { 
             minIndex = i;
             for (int j = i + 1; j < size; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
-            // Swap elements after finding the minimum
+            // ? Swap elements after finding the minimum
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
+        return arr;
     }
 
 }
